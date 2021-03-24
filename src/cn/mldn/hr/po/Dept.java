89f6a18e8eaa5022147a8947e5f6e602a527dd19
'Mr.Zhang' <'3177259856@qq.com'>
2021-03-24 11:16:28 +0800

    更改了dept

--- src/cn/mldn/hr/po/Dept.java
+++ src/cn/mldn/hr/po/Dept.java
@@ -4,4 +4,23 @@ public class Dept {
 	private String did;
 	private String dnae;
 	private String loc;
+	public String getDid() {
+		return did;
+	}
+	public void setDid(String did) {
+		this.did = did;
+	}
+	public String getDnae() {
+		return dnae;
+	}
+	public void setDnae(String dnae) {
+		this.dnae = dnae;
+	}
+	public String getLoc() {
+		return loc;
+	}
+	public void setLoc(String loc) {
+		this.loc = loc;
+	}
+	
 }
